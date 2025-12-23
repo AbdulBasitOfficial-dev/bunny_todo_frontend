6ce2aa62f34c2db4b1c5973b449382253b339338
Abdul Basit <basitbunny22@gmail.com>
2025-12-23 12:50:01 +0500

    base URL of APi Change

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,6 +1,8 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:3000';
+const API_URL =
+  import.meta.env.VITE_API_URL || "https://nest-todo-app-backend.vercel.app";
+// "http://localhost:3000"
 
 const api = axios.create({
   baseURL: API_URL,
@@ -8,7 +10,7 @@ const api = axios.create({
 
 // Request interceptor to attach JWT token
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem("token");
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
   }
